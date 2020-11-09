1cfad11b06cb74ed498c89011346c19b9ccdfc2a
Jack Travis <up938828@myport.ac.uk>
2020-11-09 23:02:43 +0000

    fixed location midpoint calculation

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ function showMap(data) {
     //maths to find the midpoint between the two given coordinates
     //adapted from and found at http://jsfiddle.net/kevinrignault/gzq64p56/
     const dLng = (data[1].lon - data[0].lon) *Math.PI/180;
-
     let lat1 = data[0].lat *Math.PI/180;
     let lat2 = data[1].lat *Math.PI/180;
     let lng1 = data[0].lon *Math.PI/180;
@@ -33,9 +32,11 @@ function showMap(data) {
     let bY = Math.cos(lat2) * Math.sin(dLng);
     let lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + bX) * (Math.cos(lat1) + bX) + bY * bY));
     let lng3 = lng1 + Math.atan2(bY, Math.cos(lat1) + bX);
+    lat3 = lat3*180/Math.PI;
+    lon3 = lng3*180/Math.PI;
 
     //initialise map on webpage
-    let location = new google.maps.LatLng(data[0].lat, data[0].lon);
+    let location = new google.maps.LatLng(lat3, lon3);
     let map = new google.maps.Map(document.getElementById('map'), {
         center: location,
         zoom: 14
